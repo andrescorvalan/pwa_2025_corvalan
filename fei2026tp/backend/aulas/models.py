d6567aa3b1a1577f766f16279e60936c7358623c
Andrés Leandro Corvalan <alcorvalan@gmail.com>
2026-06-02 00:00:38 -0300

    Paso 10: Modelos ReservaAula y HorarioMateria (uso de db_table y str formateado

--- fei2026tp/backend/aulas/models.py
+++ fei2026tp/backend/aulas/models.py
@@ -32,4 +32,34 @@ class Aula(models.Model):
     aforo = models.IntegerField(default=0)
     es_climatizada = models.BooleanField(default=False)
     def __str__(self):
-        return self.descripcion 
+        return self.descripcion
+    
+
+class ReservaAula(models.Model):
+    id_aula = models.ForeignKey(Aula, on_delete=models.CASCADE, related_name='reservas')
+    fh_desde = models.DateTimeField(null=False)
+    fh_hasta = models.DateTimeField(null=False)
+    observacion = models.CharField(max_length=256, null=True, blank=True) # Se asume opcional, si el profe la quiere obligatoria sacale el null/blank
+
+    class Meta:
+        # Nombre de la tabla en postgress, para respetar el enunciado y mas claridad en el nombre de tabla.
+        # Sino django crearia aulas_reservaaula
+        db_table = 'aulas_reserva_aula'
+
+    def __str__(self):
+        return f"Reserva {self.id} - Aula {self.id_aula.descripcion}"
+
+
+class HorarioMateria(models.Model):
+    id_materia = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name='horarios')
+    id_reserva = models.ForeignKey(ReservaAula, on_delete=models.CASCADE, related_name='horarios')
+    fh_desde = models.DateTimeField(null=False)
+    fh_hasta = models.DateTimeField(null=False)
+
+    class Meta:
+        # Nombre de la tabla en postgress, para respetar el enunciado y mas claridad en el nombre de tabla.
+        # Sino django crearia aulas_reservaaula
+        db_table = 'aulas_horario_materia'
+
+    def __str__(self):
+        return f"Horario {self.id} - Materia {self.id_materia.nombre}"
